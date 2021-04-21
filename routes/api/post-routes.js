12362ef24cb756d801df98303e674ea2f6276410
Graham Hall <ghall89@me.com>
2021-04-21 16:53:50 -0400

    refactored votes code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -71,33 +71,14 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-	Vote.create({
-		user_id: req.body.user_id,
-		post_id: req.body.post_id
-	})
-	.then(() => {
-		return Post.findOne({
-			where: {
-				id: req.body.post_id
-			},
-			attributes: [
-				'id',
-				'post_url',
-				'title',
-				'created_at',
-				[
-					sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-					'vote_count'
-				]
-			]
-		})
+	Post.upvote(req.body, { Vote })
 		.then(dbPostData => res.json(dbPostData))
 		.catch(err => {
 			console.log(err);
 			res.status(400).json(err);
 		});
-	});
 });
+
 
 router.put('/:id', (req, res) => {
 	Post.update(
